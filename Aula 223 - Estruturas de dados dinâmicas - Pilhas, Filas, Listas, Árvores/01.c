1ce7ef2c745dae232e73c5d01599f8b76a04cc91
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2023-01-12 21:07:41 -0300

    12/01

---
+++ Aula 223 - Estruturas de dados dinâmicas - Pilhas, Filas, Listas, Árvores/01.c
@@ -0,0 +1,18 @@
+/*Vetores e matrize em C são estáticos uma vez criados não da para aumentar seu tamanho.
+
+Estruturas de dados dinâmicas:
+  Uma estrutura de dados dinâminca ela é fléxivel ela pode crescer e diminuir, podemos inserir e remover itens 
+
+Estruturas mais conhecidas:
+-> Pilhas
+    -> Toda operação feita em uma pilha será na parte do topo
+        - Em uma estrutura pilha temos duas operações:
+            -> Push - empilhar, adcionar alguém na pilha 
+            -> Pop - Desempilhar, remover alguém da pilha
+
+-> Filas
+-> Listas encadeadas
+-> Árvores
+
+
+*/
--- Aulas/Aulas 101 até 150/Aula 103 - Como gerar números aleatórios com as funções rand e srand/01.c
+++ Aulas/Aulas 101 até 150/Aula 103 - Como gerar números aleatórios com as funções rand e srand/01.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <time.h>
 /*
         Aula 103 - Como gerar números aleatórios para preencher um vetor?
 */
+
 int main() {
 
     int i;
@@ -14,7 +15,7 @@ int main() {
     // Para não precisarmos usar um número pelo usuário, usamos a variável que tem o valor da hora so sistema
 
     for(int i = 0; i < 10; i++)
-        vetor[i] = rand();   // Função rand() serve para gerar números aletórios , na computação não existe números aleatórios, mas sim pseudos aleatórios. A função rand() gera um número pseudo aleátorio a parti de uma semente que é um valor passando pelo usuário. Se caso não passarmos esse número ele vai pegar algum valor da memória e sempre gerar o mesmo valor.
+        vetor[i] = rand();   // Função rand() serve para gerar números "aletórios". Na computação não existe números aleatórios, mas sim pseudos aleatórios. A função rand() gera um número pseudo aleátorio a parti de uma semente que é um valor passando pelo usuário. Se caso não passarmos esse número ele vai pegar algum valor da memória e sempre gerar o mesmo valor.
 
     printf("\n");
     for (int i = 0; i < 10; i++)
